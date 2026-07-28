11f965c00334c814aceb27ad16c90074717cbb53
Edis Aviles <edisaviles0406@gmail.com>
2026-07-27 22:56:19 -0700

    Fix database connection URL

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from .config import settings
 #structure for a postgres database
 #SQLALCHEMYDATABASE_URL = 'postgresql://<username>:<password>@<ip-address/hostname>/<database_name>'
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost/socialmedia '
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name} '
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
